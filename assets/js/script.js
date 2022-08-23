14a6ddcddfde8d6e8fbb3135f81ead3f71539d49
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-08-22 18:42:49 -0600

    created the icons for the 5 next days

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ var runFetch = function () {
       "&key=436627ee4fdb426bac05bc9393319814"
   )
     .then(function (response) {
-      console.log(response);
+      //console.log(response);
       return response.json();
     })
     .then(function (data) {
@@ -60,7 +60,7 @@ var runFetch = function () {
     });
 
   fetch(
-    "https://api.weatherbit.io/v2.0/forecast/daily&days=[5]?city=" +
+    "https://api.weatherbit.io/v2.0/forecast/daily&days=[6]?city=" +
       cityEl +
       "&key=436627ee4fdb426bac05bc9393319814"
   )
@@ -68,46 +68,96 @@ var runFetch = function () {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      //console.log(data);
       //for (var i = 0; i <= 5; i++) {
-      date1.text(data.data[0].datetime);
+      date1.text(data.data[1].datetime);
       //date1.text(moment().format("MMM Do YYYY"));
-      temp1.text("temp: " + data.data[0].temp + " celsius");
-      wind1.text("wind: " + data.data[0].wind_spd + " kmph");
-      humidity1.text("humidity: " + data.data[0].rh + "%");
+      temp1.text("temp: " + data.data[1].temp + " celsius");
+      wind1.text("wind: " + data.data[1].wind_spd + " kmph");
+      humidity1.text("humidity: " + data.data[1].rh + "%");
 
       //date2.text(moment().format("MMM Do YYYY"));
-      date2.text(data.data[1].datetime);
-      temp2.text("temp: " + data.data[1].temp + " celsius");
-      wind2.text("wind: " + data.data[1].wind_spd + " kmph");
-      humidity2.text("humidity: " + data.data[1].rh + "%");
+      date2.text(data.data[2].datetime);
+      temp2.text("temp: " + data.data[2].temp + " celsius");
+      wind2.text("wind: " + data.data[2].wind_spd + " kmph");
+      humidity2.text("humidity: " + data.data[2].rh + "%");
 
-      date3.text(data.data[2].datetime);
-      temp3.text("temp: " + data.data[2].temp + " celsius");
-      wind3.text("wind: " + data.data[2].wind_spd + " kmph");
-      humidity3.text("humidity: " + data.data[2].rh + "%");
+      date3.text(data.data[3].datetime);
+      temp3.text("temp: " + data.data[3].temp + " celsius");
+      wind3.text("wind: " + data.data[3].wind_spd + " kmph");
+      humidity3.text("humidity: " + data.data[3].rh + "%");
 
-      date4.text(data.data[3].datetime);
-      temp4.text("temp: " + data.data[3].temp + " celsius");
-      wind4.text("wind: " + data.data[3].wind_spd + " kmph");
-      humidity4.text("humidity: " + data.data[3].rh + "%");
+      date4.text(data.data[4].datetime);
+      temp4.text("temp: " + data.data[4].temp + " celsius");
+      wind4.text("wind: " + data.data[4].wind_spd + " kmph");
+      humidity4.text("humidity: " + data.data[4].rh + "%");
 
-      date5.text(data.data[4].datetime);
-      temp5.text("temp: " + data.data[4].temp + " celsius");
-      wind5.text("wind: " + data.data[4].wind_spd + " kmph");
-      humidity5.text("humidity: " + data.data[4].rh + "%");
+      date5.text(data.data[5].datetime);
+      temp5.text("temp: " + data.data[5].temp + " celsius");
+      wind5.text("wind: " + data.data[5].wind_spd + " kmph");
+      humidity5.text("humidity: " + data.data[5].rh + "%");
       $("#card-section").show();
       //  }
     });
+
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
       cityEl +
-      "&appid=" +
-      ec485387bf797030b1808247d1c50ca3
-  ).then(function (response) {
-    return response.json();
-    console.log(response);
-  });
+      "&appid=ec485387bf797030b1808247d1c50ca3"
+  )
+    .then(function (response) {
+      //console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var img =
+        "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+      $("#wicon").attr("src", img);
+    });
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityEl +
+      "&cnt=6" +
+      "&appid=ec485387bf797030b1808247d1c50ca3"
+  )
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var img1 =
+        "http://openweathermap.org/img/w/" +
+        data.list[1].weather[0].icon +
+        ".png";
+      $("#wicon1").attr("src", img1);
+
+      var img2 =
+        "http://openweathermap.org/img/w/" +
+        data.list[2].weather[0].icon +
+        ".png";
+      $("#wicon2").attr("src", img2);
+
+      var img3 =
+        "http://openweathermap.org/img/w/" +
+        data.list[3].weather[0].icon +
+        ".png";
+      $("#wicon3").attr("src", img3);
+
+      var img4 =
+        "http://openweathermap.org/img/w/" +
+        data.list[4].weather[0].icon +
+        ".png";
+      $("#wicon4").attr("src", img4);
+
+      var img5 =
+        "http://openweathermap.org/img/w/" +
+        data.list[5].weather[0].icon +
+        ".png";
+      $("#wicon5").attr("src", img5);
+    });
 };
 
 var today = moment();
